f2f16727a80827711417c1c898931936a47bd950
Matthew Clancy <mmclancy01@yahoo.com>
2024-01-19 13:55:05 -0500

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+  }
+  function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+  }
+  function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+  }
+  function appendCat(name) {
+    return [...cats, name];
+  }
+  function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+  }
+  function removeLastCat(name) {
+    return cats.slice(0, -1);
+  }
+  function removeFirstCat(name) {
+   return cats.slice(1);
+  }
